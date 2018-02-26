a526519bf8bd302ec000f698dccd494830a46a63
NukeOfTheShadow <flori.faber@gmail.com>
2018-02-26 14:48:49 +0100

    Update macchanger.sh

--- macchanger.sh
+++ macchanger.sh
@@ -6,7 +6,7 @@ iface="eth0"
 #shut down interface
 ifconfig $iface down
 
-#random mac
+#generate random mac
 MACAddress=$(dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\).*$/00:\2:\3:\4:\5:\6/')
 
 #change mac
